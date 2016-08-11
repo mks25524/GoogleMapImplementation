9234a8ce98d3b1e4d5782ecb5d86d781f2745917
mks25524 <mks25524withme@gmail.com>
2016-08-12 03:08:44 +0600

    initialize googlemap & complete home activity task

--- app/src/main/java/com/example/mks/googlemapimplementation/MainActivity.java
+++ app/src/main/java/com/example/mks/googlemapimplementation/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.mks.googlemapimplementation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends AppCompatActivity {
+static final LatLng mapImplement=new LatLng(21,57);
+    private GoogleMap googleMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+            if(googleMap==null){
+                googleMap=((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+            }
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            Marker TP=googleMap.addMarker(new MarkerOptions().position(mapImplement).title("Mahbub"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
